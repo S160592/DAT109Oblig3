3c2ac13d0eca05b041d51043381e109e5ce23ddc
s160592 <borgargrande@gmail.com>
2020-03-27 12:11:53 +0100

    levering

--- DAT109Oblig3/src/no/hvl/dat109/Servlet/RegistrerProduktForLevering.java
+++ DAT109Oblig3/src/no/hvl/dat109/Servlet/RegistrerProduktForLevering.java
@@ -1,11 +1,25 @@
 package no.hvl.dat109.Servlet;
 
 import java.io.IOException;
+
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import no.hvl.dat109.EAO.BrukarEAO;
+import no.hvl.dat109.EAO.LeveringsoversiktEAO;
+import no.hvl.dat109.EAO.ProduktEAO;
+import no.hvl.dat109.Entity.Brukar;
+import no.hvl.dat109.Entity.Leveringsoversikt;
+import no.hvl.dat109.Entity.Produkt;
+import no.hvl.dat109.hjelpeklasser.Melding;
+import no.hvl.dat109.hjelpeklasser.Meldingstype;
 
 /**
  * Servlet implementation class RegistrerProduktForLevering
@@ -14,6 +28,14 @@ import javax.servlet.http.HttpServletResponse;
 public class RegistrerProduktForLevering extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
+	@EJB
+	LeveringsoversiktEAO leveringsoversiktEAO;
+	
+	@EJB
+	BrukarEAO brukarEAO;
+	
+	@EJB
+	ProduktEAO produktEAO;
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -27,6 +49,9 @@ public class RegistrerProduktForLevering extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+	
+		
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
@@ -35,7 +60,26 @@ public class RegistrerProduktForLevering extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		String strekkode = request.getParameter("strekkode");
+		String telefon = request.getParameter("telefon");
+		
+		Brukar brukar = brukarEAO.hentBrukar(telefon);
+		Produkt produkt = produktEAO.getProdukt(strekkode);
+		
+		Leveringsoversikt leveringsoversikt = new Leveringsoversikt();
+		leveringsoversikt.setProduktBean(produkt);	
+		leveringsoversikt.setBrukarBean(brukar);
+		leveringsoversikt.setLevert(false);
+		leveringsoversiktEAO.addLeveringsoversikt(leveringsoversikt);
+	
+		
+		Melding melding = new Melding(Meldingstype.RegistreringOK);
+		
+		Gson gson = new GsonBuilder()
+		        .excludeFieldsWithoutExposeAnnotation()
+		        .create();
+		
+		response.getWriter().append(gson.toJson(melding));
 	}
 
 }
